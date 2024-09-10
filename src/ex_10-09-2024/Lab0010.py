7abe6d66ab9d198428b1ad04903aa4a0c942c15f
Minal <patilminal3597@gmail.com>
2024-09-10 18:16:50 +0530

    Program->10-09-2024

---
+++ src/ex_10-09-2024/Lab0010.py
@@ -0,0 +1,15 @@
+
+score = int(input("Enter your score: "))  # Getting the score input from the user
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Your grade is:", grade)
